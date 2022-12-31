5b78ed72fd8ee2b72ba499ed8a394b56672ff9a1
Arif Enes Aydın <72702860+b21992794@users.noreply.github.com>
2022-12-31 18:16:24 +0300

    recom with comment analysis

--- surprise_helper.py
+++ surprise_helper.py
@@ -1,11 +1,14 @@
+# This py file is a collection of methods that is used to show recommended, taken, or similar courses
 # Some of these methods directly taken from scikit-surprise documentation
 # https://surprise.readthedocs.io/en/stable/FAQ.html
 
 from collections import defaultdict
 import random
-from surprise import Dataset, KNNBasic
+from surprise import Dataset, KNNBaseline
+import numpy as np
+import pandas as pd
 
-def get_top_n(predictions, n=3):
+def get_top_n(predictions, course_df, n=5):
     # First map the predictions to each user.
     top_n = defaultdict(list)
     for uid, iid, true_r, est, _ in predictions:
@@ -15,8 +18,22 @@ def get_top_n(predictions, n=3):
     for uid, user_ratings in top_n.items():
         user_ratings.sort(key=lambda x: x[1], reverse=True)
         top_n[uid] = user_ratings[:n]
+        
+    top_courses = list(top_n.values())[0]
+    pred_courses = np.array([x[0] for x in top_courses])
+    pred_ratings = np.array([x[1] for x in top_courses])
+    ratings_sorted = pred_ratings[np.argsort(pred_courses)]
+    
+    df = course_df[course_df["id"].isin(pred_courses)].copy()
+    df["pred_rates"] = ratings_sorted
+    df.sort_values(by=['pred_rates'],inplace=True,ascending = False)
 
-    return top_n
+    return df.drop(columns=["pred_rates"])
+
+def get_user_courses(raw_user_id, rates_df, courses_df):
+    courses_taken = rates_df[rates_df["user_id"] == raw_user_id]["course_id"]
+    courses_taken_df = courses_df[courses_df["id"].isin(courses_taken)]
+    return courses_taken_df
 
 def get_anti_testset_user(full_train_set):
     anti_testset_user = []
